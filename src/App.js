0afadd3230edc395bd8e1d25272411afd6f58c13
jmaritato <jasonmaritato@outlook.com>
2021-10-03 21:47:30 -0400

    edits

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const john = '..\public\Screenshot_20211003-212702_Signal.jpg';
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={john} alt="logo" />
+        <img src={'..\public\john'} alt="logo" />
         
       </header>
     </div>
